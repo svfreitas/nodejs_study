05b712c081f84f8bfd88aa2342fc02640b8bcdbb
svfreitas <svfreitas@yahoo.com>
2024-07-11 20:02:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 ssss
 var str  = 'teste';
 
-//iddddaddress = { street : 'Rua', number : 123};
+//idihhhhdddaddress = { street : 'Rua', number : 123};
 // console.log(address.number);
 // var str = JSON.stringify(address);
 // var str2 =  _.upperCase(str);
